92a9c466b97b6d1398983c540e2a5b762b386791
ECIKALLES <erik.cikalleshi@ferchau.com>
2023-06-06 19:00:27 +0200

    powerpredict.csv updated

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os
+import sklearn
 
 DATASET_PATH = "."
 
 
 powerpredict = pd.read_csv(os.path.join(DATASET_PATH, "powerpredict.csv"))
-print(powerpredict.shape)
+dummies = pd.get_dummies(powerpredict)
+print(dummies.corr())
 
 
 

9c9eba2c8f0ac785af95d043f976e8142ff2f2c1
ErikCikalleshi <erik.cikalleshi@student.uibk.ac.at>
2023-06-07 09:24:48 +0200

    added potential neural network

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import os
 import sklearn
+import numpy as np
+from nn import neural_network
 
-DATASET_PATH = "."
+if __name__ == "__main__":
+    DATASET_PATH = "."
 
+    powerpredict_df = pd.read_csv(os.path.join(DATASET_PATH, "powerpredict.csv"))
+    powerpredict_df = powerpredict_df.dropna()  # drop rows with missing values
 
-powerpredict = pd.read_csv(os.path.join(DATASET_PATH, "powerpredict.csv"))
-dummies = pd.get_dummies(powerpredict)
-print(dummies.corr())
+    categorical_cols = powerpredict_df.select_dtypes(
+        include=['object']).columns  # get columns where we have to encode the data
 
+    encoded_df = pd.get_dummies(powerpredict_df, columns=categorical_cols)
 
-
+    neural_network(encoded_df)
 
 
---
+++ nn.py
@@ -0,0 +1,45 @@
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+
+
+def neural_network(data):
+    # x = features, y = target(power_consumption)
+    x = data.drop("power_consumption", axis=1)  # Features (input variables)
+    y = data["power_consumption"]  # Target variable
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    print("Training set shape:", x_train.shape, y_train.shape)
+    print("Testing set shape:", x_test.shape, y_test.shape)
+    # Create a PyTorch dataset
+    train_dataset = TensorDataset(x_train, y_train)
+
+    # Create data loaders for batch processing
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+    # Define the architecture of the neural network
+    model = nn.Sequential(
+        nn.Linear(x_train.shape[1], 64),
+        nn.ReLU(),
+        nn.Linear(64, 32),
+        nn.ReLU(),
+        nn.Linear(32, 1)
+    )
+
+    # Define the loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # Train the model
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        for inputs, targets in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+    return model

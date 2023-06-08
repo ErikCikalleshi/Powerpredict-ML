b00536ec07b8e4c5380ec127bab806b976d4216e
ErikCikalleshi <erik.cikalleshi@student.uibk.ac.at>
2023-06-08 17:34:14 +0200

    added potential neural network

--- main.py
+++ main.py
@@ -3,37 +3,36 @@ import os
 import sklearn
 import numpy as np
 import torch
+from sklearn.model_selection import train_test_split
 
 from nn import neural_network
 
 
 def get_encoded(powerpredict_df):
-    powerpredict_df = powerpredict_df.dropna()  # drop rows with missing values
-
-    categorical_cols = powerpredict_df.select_dtypes(
-        include=['object']).columns  # get columns where we have to encode the data
-
+    powerpredict_df = powerpredict_df.dropna()
+    categorical_cols = powerpredict_df.select_dtypes(include=['object']).columns
     encoded_df = pd.get_dummies(powerpredict_df, columns=categorical_cols)
     return encoded_df
 
 
 def leader_board_predict_fn(values):
-    # encoded_values = get_encoded(values)  # Encode the input values
     DATASET_PATH = "."
     powerpredict_df = pd.read_csv(os.path.join(DATASET_PATH, "powerpredict.csv"))
     encoded_values = get_encoded(powerpredict_df)
-    model = neural_network(encoded_values)  # Train the neural network on the encoded data
-    print(model)
-    print(values)
-    values = values.apply(pd.to_numeric, errors='coerce')
-    values = values.astype(np.float32)
-    values_tensor = torch.Tensor(values.values)
+    print(encoded_values["power_consumption"])
+    x = encoded_values.drop("power_consumption", axis=1)
+    y = encoded_values["power_consumption"]
+    x_train, _, y_train, _ = train_test_split(x, y, test_size=0.2, random_state=42)
 
-    # Reshape values_tensor if necessary
-    if len(values_tensor.shape) == 1:
-        values_tensor = values_tensor.unsqueeze(0)
-    print(values_tensor.shape)
-    # Make predictions using the trained model
+    values_encoded = get_encoded(values)
+    values_encoded = values_encoded.reindex(columns=x_train.columns, fill_value=0)
+
+    # Convert values_encoded to float type
+    values_encoded = values_encoded.astype(float)
+
+    values_tensor = torch.Tensor(values_encoded.values)
+
+    model = neural_network(x_train.values.astype(float), y_train.values.astype(float))
     predictions = model(values_tensor).detach().numpy()
 
     return predictions
@@ -41,17 +40,8 @@ def leader_board_predict_fn(values):
 
 if __name__ == "__main__":
     DATASET_PATH = "."
-
     powerpredict_df = pd.read_csv(os.path.join(DATASET_PATH, "powerpredict.csv"))
-    powerpredict_df = powerpredict_df.dropna()  # drop rows with missing values
-
-    categorical_cols = powerpredict_df.select_dtypes(
-        include=['object']).columns  # get columns where we have to encode the data
-
-    encoded_df = pd.get_dummies(powerpredict_df, columns=categorical_cols)
-
-    # model = neural_network(encoded_df)
-
     X_test = powerpredict_df.drop(columns=["power_consumption"])
-    y_test = powerpredict_df[["power_consumption"]]
+
     y_predicted = leader_board_predict_fn(X_test)
+    print(y_predicted)
--- nn.py
+++ nn.py
@@ -6,25 +6,9 @@ import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 
 
-def neural_network(data):
-    # x = features, y = target(power_consumption)
-    x = data.drop("power_consumption", axis=1)  # Features (input variables)
-    y = data["power_consumption"]  # Target variable
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    print("Training set shape:", x_train.shape, y_train.shape)
-    print("Testing set shape:", x_test.shape, y_test.shape)
-    # Convert data to float type
-    x_train = x_train.astype(np.float32)
-    y_train = y_train.astype(np.float32)
-
-    # Convert to NumPy arrays
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
-
+def neural_network(x_train, y_train):
     # Create a PyTorch dataset
     train_dataset = TensorDataset(torch.Tensor(x_train), torch.Tensor(y_train))
-
 
     # Create data loaders for batch processing
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
@@ -48,9 +32,11 @@ def neural_network(data):
         for inputs, targets in train_loader:
             optimizer.zero_grad()
             outputs = model(inputs)
-            targets = targets.view(-1, 1) # reshape the targets to match the shape of outputs
+            targets = targets.view(-1, 1)  # reshape the targets to match the shape of outputs
             loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
-    
+
     return model
+
+
